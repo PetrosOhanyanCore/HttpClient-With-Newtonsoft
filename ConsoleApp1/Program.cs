37bc64d3c4899478a83039688a612ed07abdf81d
Petros Ohanyan <petrosmu24@gmail.com>
2023-12-02 14:56:06 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleApp1;
+using System.Net.Mime;
+using System.Text;
+using Newtonsoft.Json;
+
+RequestModel rm = new RequestModel();
+
+var json = JsonConvert.SerializeObject(rm);
+var obj = JsonConvert.DeserializeObject<RequestModel>(json);
+
+
+using (HttpClient cl = new HttpClient())
+{
+    var request = new HttpRequestMessage
+    {
+        Method = HttpMethod.Post,
+        RequestUri = new Uri("https://localhost:44356/Auth/Register"),
+        Content = new StringContent(json, Encoding.UTF8, MediaTypeNames.Application.Json /* or "application/json" in older versions */),
+    };
+
+    var response = await cl.SendAsync(request).ConfigureAwait(false);
+    response.EnsureSuccessStatusCode();
+
+    var responseBody = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+    Console.WriteLine(responseBody);
+}
---
+++ ConsoleApp1/RequestModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class RequestModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public RequestModel()
+        {
+            Email = "someEmail3@gmail.com";
+            Password = "Some3.com";
+            ConfirmPassword = "Some3.com";
+            FirstName = "Someemail";
+            LastName = "Someemail";
+        }
+    }
+}
